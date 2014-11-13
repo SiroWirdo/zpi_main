a8bb8003efd6d6ddcf005335721bb2258705287e
192190 <192190@p333b11.ii.local>
2014-11-13 11:24:25 +0100

    Modyfikacja edycji kierowców

--- ZPI/src/main/java/admin/driver/edit/view/ModifyDriverView.java
+++ ZPI/src/main/java/admin/driver/edit/view/ModifyDriverView.java
@@ -1,23 +1,114 @@
 package admin.driver.edit.view;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import admin.driver.edit.controller.ModifyDriverController;
 import admin.driver.edit.model.ModifyDriverModel;
 
 
 public class ModifyDriverView extends JFrame{
-	ModifyDriverController modifyDriverController;
-	ModifyDriverModel modifyDriverModel;
-	
-	
+	private ModifyDriverController modifyDriverController;
+	private ModifyDriverModel modifyDriverModel;
+	private JLabel jlName;
+	private JLabel jlSurname;
+	private JLabel jlPhoneNumber;
+	private JLabel jlLicenseNumber;
+	private JLabel jlPesel;
+	private JLabel jlStatus;
+	private JLabel jlCarId;
+	private JLabel jlUserId;
+	private JTextField tfName;
+	private JTextField tfSurname;
+	private JTextField tfPhoneNumber;
+	private JTextField tfLicenseNumber;
+	private JTextField tfPesel;
+	private JTextField tfStatus;
+	private JTextField tfCarId;
+	private JTextField tfUserId;
+	private JButton edit;
+	private JButton cancel;
+	private JPanel mainPanel;
+
+
 	public ModifyDriverView(ModifyDriverController modifyDriverController, ModifyDriverModel modifyDriverModel){
 		this.modifyDriverController = modifyDriverController;
 		this.modifyDriverModel = modifyDriverModel;
 
 	}
-	
+
 	public void initialize(){
+		this.setBounds(100, 100, 800, 600);
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+		int x = 10;
+		int y = 10;
+
+		mainPanel = new JPanel();
+		mainPanel.setLayout(null);
+		this.add(mainPanel);
+
+		jlName = new JLabel("Imiê: ");
+		jlName.setBounds(x, y, 60, 20);
+		mainPanel.add(jlName);
+
+		tfName = new JTextField();
+		tfName.setBounds(x + 70, y, 100, 20);
+		mainPanel.add(tfName);
+
+		jlSurname = new JLabel("Nazwisko: ");
+		jlSurname.setBounds(x, y=y+30, 60, 20);
+		mainPanel.add(jlName);
+
+		tfSurname = new JTextField();
+		tfSurname.setBounds(x + 70, y, 100, 20);
+		mainPanel.add(tfSurname);
+
+		jlPhoneNumber = new JLabel("Telefon: ");
+		jlPhoneNumber.setBounds(x, y=y+30, 60, 20);
+		mainPanel.add(jlPhoneNumber);
+
+		tfPhoneNumber = new JTextField();
+		tfPhoneNumber.setBounds(x+70, y, 100, 20);
+		mainPanel.add(tfPhoneNumber);
+
+		jlLicenseNumber = new JLabel("Licencja: ");
+		jlLicenseNumber.setBounds(x, y=y+30, 60, 100);
+		mainPanel.add(jlLicenseNumber);
+
+		tfLicenseNumber = new JTextField();
+		tfLicenseNumber.setBounds(x+70, y, 100, 20);
+		mainPanel.add(tfLicenseNumber);
+
+		jlPesel = new JLabel("PESEL: ");
+		jlPesel.setBounds(x, y=y+30, 60, 20);
+		mainPanel.add(jlPesel);
+
+		tfPesel = new JTextField();
+		tfPesel.setBounds(x+70, y, 100, 20);
+		mainPanel.add(tfPesel);
+
+		jlStatus = new JLabel("Status: ");
+		jlStatus.setBounds(x, y=y+30, 60, 20);
+		mainPanel.add(jlStatus);
+
+		tfStatus = new JTextField();
+		tfStatus.setBounds(x+70, y, 100, 20);
+		mainPanel.add(tfStatus);
+
+		jlCarId = new JLabel("ID samochodu: ");
+		jlCarId.setBounds(x, y=y+30, 120, 20);
+		mainPanel.add(jlCarId);
 		
+		tfCarId = new JTextField();
+		tfCarId.setBounds(x+130, y, 100, 20);
+		mainPanel.add(tfCarId);
+
+
+
+		this.setVisible(true);
 	}
 }
--- ZPI/src/main/java/algorithm/Algorithm.java
+++ ZPI/src/main/java/algorithm/Algorithm.java
@@ -19,99 +19,100 @@ import org.parse4j.ParsePush;
 import org.parse4j.ParseQuery;
 
 public class Algorithm implements Runnable {
-	List<Driver> drivers;
-	Order order;
+    List<Driver> drivers;
+    Order order;
 
-	public boolean chooseDriver(Order order){
-		// TODO  tutaj stworzenie listy wszystkich dostêpnych kierowców. Wywo³anie metod sortuj¹cych i rozpoczêcie powiadamiania kierowców.
-		ParseQuery<Driver> query = ParseQuery.getQuery(Driver.class);
-		query.whereEqualTo("status", 0);
-		try {
-			drivers = query.find();
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    public boolean chooseDriver(Order order){
+        // TODO  tutaj stworzenie listy wszystkich dostêpnych kierowców. Wywo³anie metod sortuj¹cych i rozpoczêcie powiadamiania kierowców.
+        ParseQuery<Driver> query = ParseQuery.getQuery(Driver.class);
+        query.whereEqualTo("status", 0);
+        try {
+            drivers = query.find();
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
-		//Mo¿na zrobiæ coœ bardziej wydajnego ni¿ to chyba
-		Collections.sort(drivers, new Comparator<Driver>(){
+        //Mo¿na zrobiæ coœ bardziej wydajnego ni¿ to chyba
+        Collections.sort(drivers, new Comparator<Driver>(){
 
-			@Override
-			public int compare(Driver arg0, Driver arg1) {
-				//nie jestem pewien czy to zadzia³a ale chyba tak
-				return Double.compare(countDistanceInStraightLine(arg0.getCar()), countDistanceInStraightLine(arg1.getCar()));
-			}
+            @Override
+            public int compare(Driver arg0, Driver arg1) {
+                //nie jestem pewien czy to zadzia³a ale chyba tak
+                return Double.compare(countDistanceInStraightLine(arg0.getCar()), countDistanceInStraightLine(arg1.getCar()));
+            }
 
-		});
+        });
 
-		// TODO tutaj wybieramy kilka pierwszych z listy i ich sortujemy wed³ug drogi rzeczywistej
+        // TODO tutaj wybieramy kilka pierwszych z listy i ich sortujemy wed³ug drogi rzeczywistej
 
-		ArrayList<Driver> sortedDrivers = new ArrayList<Driver>();
-		for(int i = 0; i < 6; i++){
-			sortedDrivers.add(drivers.get(i));
-		}
+        ArrayList<Driver> sortedDrivers = new ArrayList<Driver>();
+        for(int i = 0; i < 6; i++){
+            sortedDrivers.add(drivers.get(i));
+        }
 
-		this.order = order;
+        this.order = order;
 
-		this.run();
+        this.run();
 
-		return true;
-	}
+        return true;
+    }
 
-	public double countDistanceInStraightLine(Car car){
-		ParseGeoPoint driverPosition = car.getCurrentPosition();
-		ParseGeoPoint clientPosition = order.getPickupAddressGeo();
+    public double countDistanceInStraightLine(Car car){
+        ParseGeoPoint driverPosition = car.getCurrentPosition();
+        ParseGeoPoint clientPosition = order.getPickupAddressGeo();
 
-		double distance = driverPosition.distanceInKilometersTo(clientPosition);
-		return distance;
-	}
+        double distance = driverPosition.distanceInKilometersTo(clientPosition);
+        return distance;
+    }
 
-	public void notifyDriver(Driver driver){
-		ParsePush push = new ParsePush();
-		//ArrayList<String> channels = new ArrayList<String>();
-		//sprawdziæ jakie potrzebne s¹ do listy rzeczy czy: ["",id] czy [id] czy inaczej
-		ParseObject pointer = (ParseObject) driver.getParseObject("userId");
-		String userId = (String)pointer.getObjectId();
-		String channel = "user_" + userId;
-		push.setOrderMessage(order.getObjectId());
-		push.setChannel(channel);
-		try {
-			push.send();
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    public void notifyDriver(Driver driver){
+        //Nie wiem czy trzeba tu doda inicjalizacj bazy
+        ParsePush push = new ParsePush();
+        //ArrayList<String> channels = new ArrayList<String>();
+        //sprawdziæ jakie potrzebne s¹ do listy rzeczy czy: ["",id] czy [id] czy inaczej
+        ParseObject pointer = (ParseObject) driver.getParseObject("userId");
+        String userId = (String)pointer.getObjectId();
+        String channel = "user_" + userId;
+        push.setOrderMessage(order.getObjectId());
+        push.setChannel(channel);
+        try {
+            push.send();
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
-	@Override
-	public void run() {
+    @Override
+    public void run() {
 
-		boolean assigned = false;
-		int i = 0;
-		while(!assigned && i < drivers.size()){
-			notifyDriver(drivers.get(i));
+        boolean assigned = false;
+        int i = 0;
+        while(!assigned && i < drivers.size()){
+            notifyDriver(drivers.get(i));
 
-			try {
-				Thread.sleep(60000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+            try {
+                Thread.sleep(60000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
 
-			ParsePointer pointer = (ParsePointer) order.get("driver");
+            ParsePointer pointer = (ParsePointer) order.get("driver");
 
-			// nie wiem czy na pewno zwróci null jak nie jest przypisany
-			if(pointer != null){
-				assigned = true;
-			}else{
-				i++;
-			}
-		}
+            // nie wiem czy na pewno zwróci null jak nie jest przypisany
+            if(pointer != null){
+                assigned = true;
+            }else{
+                i++;
+            }
+        }
 
-		if(!assigned){
-			// co jeœli nikt nie odpowie
-		}
-	}
+        if(!assigned){
+            // co jeœli nikt nie odpowie
+        }
+    }
 
 
 }
